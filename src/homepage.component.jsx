45f6f06cba5c3c0e1a29dcc049d8ea3bc3b982ff
Hedi M Sharif <hedymuhamad461@gmail.com>
2021-12-13 11:29:09 +0300

    Construct homepage directory component and it's items using raw html

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import './homepage.styles.scss'
+import './homepage.styles.scss';
 
-const HomePage = () => {
+const HomePage = () =>{
     return(
         <div className='home-page'>
-            Startup!
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='info-container'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='info-container'>
+                        <h1 className='title'>JACKETS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='info-container'>
+                        <h1 className='title'>SNEAKERS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='info-container'>
+                        <h1 className='title'>WOMENS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='info-container'>
+                        <h1 className='title'>MENS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
