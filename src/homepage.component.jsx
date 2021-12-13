f541b9b029f907d2f9537e17d2625e8d60e073c1
Hedi M Sharif <hedymuhamad461@gmail.com>
2021-12-13 11:25:02 +0300

    Startup!
    installing package 'sass'

---
+++ src/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+const HomePage = () => {
+    return(
+        <div className='home-page'>
+            Startup!
+        </div>
+    )
+}
+
+export default HomePage;
