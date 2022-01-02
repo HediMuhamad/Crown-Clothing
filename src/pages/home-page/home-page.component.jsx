238e0f3c5c83b5f7ec844ccb92425ac295ea051b
Hedi M Sharif <hedymuhamad461@gmail.com>
2022-01-03 01:30:55 +0300

    Prevent user from visiting '/account' link while signed in

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './homepage.styles.scss';
+import './home-page.styles.scss';
 import DirectoryMenu from '../../components/directory-menu/directory-menu.component';
 const HomePage = () =>{
     return(
